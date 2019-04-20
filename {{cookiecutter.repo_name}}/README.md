45e90c9a2fce74eb7c59030be1a799e49a4f7f6d
Matthew Purdon <matthew@codenaked.org>
2019-04-19 23:33:33 -0400

    added setup.py and minor improvements

--- {{cookiecutter.repo_name}}/README.md
+++
@@ -1,2 +0,0 @@
-# script_base
-A base for developing one-off scripts using docker containers and stuff
---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+try:
+    # Allow installing package without any Cython available. This
+    # assumes you are going to include the .c files in your sdist.
+    import Cython
+except ImportError:
+    Cython = None
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+setup(
+    name='{{ cookiecutter.distribution_name }}',
+    version='{{ cookiecutter.version }}',
+    license='{{ cookiecutter.license }}',
+    description={{ '{0!r}'.format(cookiecutter.project_short_description).lstrip('ub') }},
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
+    author={{ '{0!r}'.format(cookiecutter.full_name).lstrip('ub') }},
+    author_email={{ '{0!r}'.format(cookiecutter.email).lstrip('ub') }},
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+{%- if cookiecutter.license in ["BSD 2-Clause License", "BSD 3-Clause License"] %}
+        'License :: OSI Approved :: BSD License',
+{%- elif cookiecutter.license == "MIT license" %}
+        'License :: OSI Approved :: MIT License',
+{%- elif cookiecutter.license == "Public Domain" %}
+        'License :: Public Domain',
+{%- endif %}
+
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
+    project_urls={
+{%- if cookiecutter.sphinx_docs == "yes" %}
+        'Documentation': 'https://{{ cookiecutter.repo_name|replace('.', '') }}.readthedocs.io/',
+        'Changelog': 'https://{{ cookiecutter.repo_name|replace('.', '') }}.readthedocs.io/en/latest/changelog.html',
+{%- else %}
+        'Changelog': 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}/blob/master/CHANGELOG.rst',
+{%- endif %}
+        'Issue Tracker': 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}/issues',
+    },
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    python_requires=['>=3.6',],
+    install_requires=[
+{%- if cookiecutter.command_line_interface == 'click' %}
+        'click',
+{%- endif %}
+    ],
+    extras_require={
+    },
+{%- if cookiecutter.command_line_interface != 'no' %}
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.command_line_interface_bin_name }} = {{ cookiecutter.package_name }}.cli:main',
+        ]
+    },
+{%- endif %}
+)
